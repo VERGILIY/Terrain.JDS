c44378a32fa24542e468799e3929721d836e7fce
VERGILIY <Jakovlev.DS@gmail.com>
2012-05-01 13:03:36 +0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TerrainJds::Application.config.secret_token = '9560c26f6a79f93ad19328b8dd9715ea3eac06ce5eb167700b8f85dfc80de642bc4e8279d67a051edc00cce1f49572b7fad4eba123e756450cec96ccc58f8b4f'
