1b069a549df463b5b10825fe13b44596ce576fe1
VERGILIY <Jakovlev.DS@gmail.com>
2012-05-01 22:15:00 +0800

    create 3D scene with generated terrain

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,6 +10,10 @@
 // WARNING: THE FIRST BLANK LINE MARKS THE END OF WHAT'S TO BE PROCESSED, ANY BLANK LINE SHOULD
 // GO AFTER THE REQUIRES BELOW.
 //
-//= require jquery
-//= require jquery_ujs
-//= require_tree .
+//= require Three.js
+//= require FirstPersonNavigationControls.js
+//= require RequestAnimationFrame.js
+//= require TerrainGeneration.js
+//= require Scene.js
+//= require Panel.js
+//= require front.js
